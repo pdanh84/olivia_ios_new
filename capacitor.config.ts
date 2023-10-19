7ba99a34c6b24cad80c49b78bfac57728b856756
Apple <apple@Apples-MacBook-Pro.local>
2023-10-19 15:55:01 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'olivia_ios_new',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
